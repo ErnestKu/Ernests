71c11241ea39e926bb88ffe647dace99861982aa
Ernests <145021221+ErnestKu@users.noreply.github.com>
2023-10-05 15:30:58 +0300

    Create p2.uzdevums.py

---
+++ p2.uzdevums.py
@@ -0,0 +1,14 @@
+Dēlis =(input("ievadi kādu dēli tu gribi ja gribi pa 145e ievadi x ja gribi pa 125e ievadi y"))
+print(Dēlis)
+x= ("dēlis pa 145e")
+y= ("dēlis pa 125e")
+print(x)
+print(y)
+
+plat = int(input("ievadiet platumu:"))
+biez = int(input("ievadiet biezumu:"))
+gar = int(input("ievadiet garumu:"))
+ 
+res = plat * biez * gar
+ 
+print("tilpums ir ", res)
