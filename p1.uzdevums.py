f2d42b4c183735c78445603197f7202f7bd5e44e
Ernests <145021221+ErnestKu@users.noreply.github.com>
2023-10-05 15:30:26 +0300

    Create p1.uzdevums.py

---
+++ p1.uzdevums.py
@@ -0,0 +1,10 @@
+Vārds =(input("ievadi savu vārdu"))
+print(Vārds)
+Mēnesī =(input("ievadi cik h strādā mēnesī"))
+print(Mēnesī)
+Likme =(input("ievadi cik saņem par 1 h"))
+print(Likme)
+
+def alga (Likme)
+    if Likme > 40
+    alga = 40 * Likme * Mēnesī
