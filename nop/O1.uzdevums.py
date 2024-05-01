9e7b030d2cfb8b7e80eebc17afa83db7f13dced6
Ernests <145021221+ErnestKu@users.noreply.github.com>
2024-05-01 21:50:19 +0300

    Add files via upload

---
+++ nop/O1.uzdevums.py
@@ -0,0 +1,15 @@
+Vārds =(input("ievadi vārdu"))
+print(Vārds)
+vārds = [".jpg ir attēls", ".gif ir attēls", ".bmp ir attēls"]
+
+for x in Vārds:
+  for y in vārds:
+    print(x, y)
+    pirmvārds = ["rudens"]
+print(pirmvārds)
+vārds = [".txt ir dokuments", ".doc ir dokuments", ".pdf ir dokuments"]
+
+for x in pirmvārds:
+  for y in vārds:
+    print(x, y)
+    
